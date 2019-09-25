cc15ffbb9e4df47b6b47a8199b8d2ec217e36bba
james.patrick <jpatrick1@gmail.com>
2019-09-24 22:09:05 -0400

    added documentation to swagger api

--- server/mathfinder/api.py
+++ server/mathfinder/api.py
@@ -2,105 +2,108 @@
 # -*- coding: utf-8 -*-
 
 API = {
-	"openapi": "3.0.0",
-	"info": {
-		"title": "MathFinder",
-		"version": "1",
-		"description": "HTTP Interface for MathFinder"
-	},
-	"paths": {
-		"/runMathFinder": {
-			"post": {
-				"summary": "Returns Rect Information",
-				"tags": [
-					"MathFinder"
-				],
-				"operationId": "mathfinder.endpoint.math_finder",
-				"requestBody": {
-					"content": {
-						"multipart/form-data": {
-							"schema": {
-								"type": "object",
-								"properties": {
-									"input": {
-										"type": "string",
-										"format": "binary"
-									}
-								},
-								"required": [
-									"input"
-								]
-							}
-						}
-					}
-				},
-				"responses": {
-					"200": {
-						"description": "Rect JSON",
-						"content": {
-							"application/json": {
-								"schema": {
-									"type": "object",
-									"properties": {}
-								}
-							}
-						}
-					}
-				}
-			}
-		},
-		"/displayDetections": {
-			"post": {
-				"summary": "Returns image with detections overlayed",
-				"tags": [
-					"MathFinder"
-				],
-				"operationId": "mathfinder.endpoint.display_detections",
-				"requestBody": {
-					"content": {
-						"multipart/form-data": {
-							"schema": {
-								"type": "object",
-								"properties": {
-									"image": {
-										"type": "string",
-										"format": "binary"
-									},
-									"detections": {
-										"type": "string",
-										"format": "binary"
-									}
-								},
-								"required": [
-									"image",
-									"detections"
-								]
-							},
-							"encoding": {
-								"detections": {
-									"contentType": "application/json"
-								},
-								"image": {
-									"contentType": "image/png, image/jpeg"
-								}
-							}
-						}
-					}
-				},
-				"responses": {
-					"200": {
-						"description": "Image",
-						"content": {
-							"image/jpeg": {
-								"schema": {
-									"type": "string",
-									"format": "binary"
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    "openapi": "3.0.0",
+    "info": {
+        "title": "MathFinder",
+        "version": "1",
+        "description": "HTTP Interface for MathFinder"
+    },
+    "paths": {
+        "/runMathFinder": {
+            "post": {
+                "summary": "Returns Rect Information",
+                "tags": [
+                    "MathFinder"
+                ],
+                "operationId": "mathfinder.endpoint.math_finder",
+                "requestBody": {
+                    "content": {
+                        "multipart/form-data": {
+                            "schema": {
+                                "type": "object",
+                                "properties": {
+                                    "input": {
+                                        "description": "`input`: __{png, jpeg}__\n* Image File\n",
+                                        "type": "string",
+                                        "format": "binary"
+                                    }
+                                },
+                                "required": [
+                                    "input"
+                                ]
+                            }
+                        }
+                    }
+                },
+                "responses": {
+                    "200": {
+                        "description": "Rect JSON",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "object",
+                                    "properties": {}
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "/displayDetections": {
+            "post": {
+                "summary": "Returns image with detections overlayed",
+                "tags": [
+                    "MathFinder"
+                ],
+                "operationId": "mathfinder.endpoint.display_detections",
+                "requestBody": {
+                    "content": {
+                        "multipart/form-data": {
+                            "schema": {
+                                "type": "object",
+                                "properties": {
+                                    "image": {
+                                        "description": "`image`: __{png, jpeg}__\n* Image File\n",
+                                        "type": "string",
+                                        "format": "binary"
+                                    },
+                                    "detections": {
+                                        "description": "`detections`: __{json}__\n* Detection JSON file (COCO format), see **/runMathFinder**\n",
+                                        "type": "string",
+                                        "format": "binary"
+                                    }
+                                },
+                                "required": [
+                                    "image",
+                                    "detections"
+                                ]
+                            },
+                            "encoding": {
+                                "detections": {
+                                    "contentType": "application/json"
+                                },
+                                "image": {
+                                    "contentType": "image/png, image/jpeg"
+                                }
+                            }
+                        }
+                    }
+                },
+                "responses": {
+                    "200": {
+                        "description": "Image",
+                        "content": {
+                            "image/jpeg": {
+                                "schema": {
+                                    "type": "string",
+                                    "format": "binary"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
